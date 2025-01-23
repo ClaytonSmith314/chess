3445bbfedbb2c6826e1f34dff32365adb589ba2a
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2025-01-22 19:56:50 -0700

    Implement ChessPosition and ChessBoard except resetBoard().  Changes not yet tested.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] chess_board_array = ChessPiece[8][8];
+
     public ChessBoard() {
-        
+
     }
 
     /**
@@ -19,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        chess_board_array[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +32,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return chess_board_array[position.getRow()][position.getColumn()];
     }
 
     /**
